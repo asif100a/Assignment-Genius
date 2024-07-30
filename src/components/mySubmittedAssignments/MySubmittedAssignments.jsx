1ab85a34b1efb91d814664fd26e2b404b84d6024
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-07-30 22:54:03 +0600

    My submitted assignment page style changed

--- src/components/mySubmittedAssignments/MySubmittedAssignments.jsx
+++ src/components/mySubmittedAssignments/MySubmittedAssignments.jsx
@@ -39,54 +39,56 @@ const MySubmittedAssignments = () => {
     }
 
     return (
-        <section className="container px-4 mx-auto py-12 bg-gray-50">
-            <div className="">
-                <h2 className="text-2xl md:text-3xl lg:text-4xl text-center font-semibold font-noto-serif text-gray-800 dark:text-white capitalize">My submitted assignments</h2>
-            </div>
+        <section className="bg-gray-50 min-h-[calc(100vh-413.6px)]">
+            <div className="container px-4 mx-auto py-12">
+                <div className="">
+                    <h2 className="text-2xl md:text-3xl lg:text-4xl text-center font-semibold font-noto-serif text-gray-800 dark:text-white capitalize">My submitted assignments</h2>
+                </div>
 
-            <div className="flex flex-col mt-8">
-                <div className="-mx-4 overflow-x-auto sm:-mx-6 lg:mx-8">
-                    <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                        <div className="overflow-hidden border-4 border-double border-[#808080] dark:border-gray-700 rounded-lg">
-                            <table className="min-w-full">
-                                <thead className="bg-inherit border-b-double dark:bg-gray-800">
-                                    <tr>
-                                        <th scope="col" className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
-                                            <div className="flex items-center gap-x-3">
-                                                <span className="font-noto-serif font-bold">Name</span>
-                                            </div>
-                                        </th>
+                <div className="flex flex-col mt-8">
+                    <div className="-mx-4 overflow-x-auto sm:-mx-6 lg:mx-8">
+                        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                            <div className="overflow-hidden border-4 border-double border-[#808080] dark:border-gray-700 rounded-lg">
+                                <table className="min-w-full">
+                                    <thead className="bg-inherit border-b-double dark:bg-gray-800">
+                                        <tr>
+                                            <th scope="col" className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
+                                                <div className="flex items-center gap-x-3">
+                                                    <span className="font-noto-serif font-bold">Name</span>
+                                                </div>
+                                            </th>
 
-                                        <th scope="col" className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
-                                            <span className="font-noto-serif font-bold">Assignment title</span>
-                                        </th>
+                                            <th scope="col" className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
+                                                <span className="font-noto-serif font-bold">Assignment title</span>
+                                            </th>
 
-                                        <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
-                                            <span className="font-noto-serif font-bold">Status</span>
-                                        </th>
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
+                                                <span className="font-noto-serif font-bold">Status</span>
+                                            </th>
 
-                                        <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
-                                            <span className="font-noto-serif font-bold">Assignment marks</span>
-                                        </th>
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
+                                                <span className="font-noto-serif font-bold">Assignment marks</span>
+                                            </th>
 
-                                        <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
-                                            <span className="font-noto-serif font-bold">Obtained marks</span>
-                                        </th>
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
+                                                <span className="font-noto-serif font-bold">Obtained marks</span>
+                                            </th>
 
-                                        <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
-                                            <span className="font-noto-serif font-bold">Feedback</span>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-inherit divide-y-2 divide-[#808080] dark:divide-[#808080] dark:bg-gray-900">
-                                    {
-                                        assignments?.map(assignment => <RowOfTable
-                                            key={assignment._id}
-                                            assignment={assignment}
-                                        />)
-                                    }
-                                </tbody>
-                            </table>
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-700 dark:text-gray-400">
+                                                <span className="font-noto-serif font-bold">Feedback</span>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="bg-inherit divide-y-2 divide-[#808080] dark:divide-[#808080] dark:bg-gray-900">
+                                        {
+                                            assignments?.map(assignment => <RowOfTable
+                                                key={assignment._id}
+                                                assignment={assignment}
+                                            />)
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>

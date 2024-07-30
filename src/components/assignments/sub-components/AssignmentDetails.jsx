beeb806a7c5c92a5894b9e7c42e32c6c25c0db1d
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-07-30 08:48:46 +0600

    Style changed in the assignment details page

--- src/components/assignments/sub-components/AssignmentDetails.jsx
+++ src/components/assignments/sub-components/AssignmentDetails.jsx
@@ -8,6 +8,7 @@ import useAuth from "../../../Hooks/useAuth";
 import { useRef, useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import "./assignmentDetails.css";
 
 const AssignmentDetails = () => {
     const { user } = useAuth();
@@ -95,48 +96,46 @@ const AssignmentDetails = () => {
     };
 
     return (
-        <div className="py-16 relative border border-red-600">
+        <div className="py-12 relative bg-gray-50">
             <div className="mx-auto px-6 max-w-6xl text-gray-500">
                 <div className="text-center">
-                    <h2 className="text-3xl text-gray-950 dark:text-white font-semibold">Assignment details</h2>
-                    <p className="mt-6 text-gray-700 dark:text-gray-300 w-full md:w-[36rem] mx-auto">See the details about {title} and can take assignment on {title}.</p>
+                    <h2 className="text-2xl md:text-3xl lg:text-4xl text-gray-950 dark:text-white font-semibold font-noto-serif">Assignment details</h2>
+                    <p className="mt-6 text-gray-700 font-pt-serif dark:text-gray-300 w-full md:w-[36rem] mx-auto">See the details about {title} and can take assignment on {title}.</p>
                 </div>
                 <div className="mt-12 max-w-xl mx-auto">
 
-                    <div className="relative group overflow-hidden p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
-                        <div aria-hidden="true" className="inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-sky-500 to-white dark:from-white dark:to-white blur-2xlOpacity-25 darkOpacity-5 dark:group-hoverOpacity-10"></div>
+                    <div className="relative group overflow-hidden p-8 rounded-xl bg-inherit card-border dark:bg-gray-900">
+                        <div aria-hidden="true" className="inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-gray-400 to-white dark:from-white dark:to-white blur-2xlOpacity-25 darkOpacity-5 dark:group-hoverOpacity-10"></div>
                         <div className="relative">
-                            <div className="border border-sky-500/10 flex relative *:relative *:size-12 *:m-auto size-16 rounded-lg dark:bg-gray-900 dark:border-white/15 before:rounded-[6px] before:absolute before:inset-0 before:border-t before:border-white before:from-sky-100 dark:before:border-white/20 before:bg-gradient-to-b dark:before:from-white/10 dark:before:to-transparent before:shadow dark:before:shadow-gray-950">
+                            <div className="border border-gray-500/10 flex relative *:relative *:size-12 *:m-auto size-16 rounded-lg dark:bg-gray-900 dark:border-white/15 before:rounded-[6px] before:absolute before:inset-0 before:border-t before:border-white before:from-gray-100 dark:before:border-white/20 before:bg-gradient-to-b dark:before:from-white/10 dark:before:to-transparent before:shadow dark:before:shadow-gray-950">
                                 <img src={thumbnail_img} alt="Thumbnail" className="w-12 h-12" />
                             </div>
 
                             <div className="mt-6 pb-6 rounded-b-[--card-border-radius]">
-                                <h1 className="text-2xl font-medium mb-6">{title}</h1>
-                                <p className="text-gray-700 dark:text-gray-300">{description}</p>
+                                <h1 className="text-2xl font-medium mb-6 font-pt-serif">{title}</h1>
+                                <p className="text-gray-700 dark:text-gray-300 font-pt-serif">{description}</p>
                             </div>
                             <div className="space-y-2">
                                 <div className="flex gap-2 items-center">
                                     <IoMdCheckmarkCircleOutline />
-                                    <p><span className="text-base font-semibold">Marks:</span> <span>{marks}</span></p>
+                                    <p className="font-pt-serif"><span className="text-base font-semibold">Marks:</span> <span>{marks}</span></p>
                                 </div>
                                 <div className="flex gap-2 items-center">
                                     <FaLevelUpAlt />
-                                    <p><span className="text-base font-semibold">Level:</span> <span>{level}</span></p>
+                                    <p className="font-pt-serif"><span className="text-base font-semibold">Level:</span> <span>{level}</span></p>
                                 </div>
                                 <div className="flex gap-2 items-center">
                                     <LuCalendarClock />
-                                    <p><span className="text-base font-semibold">Deadline:</span> <span>{assignmentDeadline}</span></p>
+                                    <p className="font-pt-serif"><span className="text-base font-semibold">Deadline:</span> <span>{assignmentDeadline}</span></p>
                                 </div>
                                 <div className="flex gap-2 items-center">
                                     <MdOutlineMail />
-                                    <p><span className="text-base font-semibold">Created by:</span> <span>{email}</span></p>
+                                    <p className="font-pt-serif"><span className="text-base font-semibold">Created by:</span> <span>{email}</span></p>
                                 </div>
                             </div>
                             <div className="flex gap-3 -mb-8 py-4 border-t border-gray-200 dark:border-gray-800 mt-6">
 
-                                <button onClick={handleShowModal} className="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-orange-300 rounded-lg group">
-                                    <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
-                                    <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+                                <button onClick={handleShowModal} className="relative inline-flex items-center justify-center px-5 md:px-10 py-2 md:py-4 overflow-hidden font-bold tracking-tighter text-gray-700 bg-inherit btn-border font-pt-serif ease-in-out transition duration-300 hover:bg-gray-600 hover:text-white rounded-lg group">
                                     <span className="relative">Take assignment</span>
                                 </button>
 
@@ -150,11 +149,11 @@ const AssignmentDetails = () => {
             <div ref={textRef} className="absolute w-full h-full top-0 flex justify-center items-center bg-black bg-opacity-35 hidden">
                 <div className="modal-box w-[36rem]">
                     <div className="flex justify-between">
-                        <h3 className="font-bold text-lg">Hello!</h3>
+                        <h3 className="font-bold text-gray-700 font-pt-serif text-lg">Give the marks</h3>
                         <div className="modal-action justify-start mt-0">
                             <form method="dialog">
                                 {/* if there is a button in form, it will close the modal */}
-                                <button onClick={handleHideModal} className="btn rounded-full btn-sm h-10 min-h-10 btn-outline text-red-600 border-red-600 hover:bg-red-500 hover:text-white hover:border-white"><RxCross1 /></button>
+                                <button onClick={handleHideModal} className="btn rounded-full btn-sm h-10 min-h-10 btn-outline text-red-600 border-gray-600 hover:bg-red-500 hover:text-white hover:border-white"><RxCross1 className="" /></button>
                             </form>
                         </div>
                     </div>
@@ -164,8 +163,8 @@ const AssignmentDetails = () => {
                             <input
                                 type="text"
                                 name="doc_link"
-                                placeholder="Type here your PDF or doc link"
-                                className="input input-bordered input-accent w-full"
+                                placeholder="Write your PDF or doc link"
+                                className="input input-bordered border-2 border-gray-600 w-full focus:border-gray-600 focus:outline-double focus:outline-gray-600 font-pt-serif"
                             />
                             <p className="text-red-600">{error}</p>
                         </div>
@@ -174,13 +173,13 @@ const AssignmentDetails = () => {
                             <label htmlFor="note"></label>
                             <textarea
                                 name="note"
-                                className="textarea textarea-info w-full h-36"
+                                className="textarea border-2 border-gray-600 focus:border-gray-600 focus:outline-double focus:outline-gray-600 w-full h-36 font-pt-serif"
                                 placeholder="Take your quick note"
                             ></textarea>
                             <p className="text-red-600">{error2}</p>
                         </div>
 
-                        <input type="submit" value={'Submit the assignment'} className="px-3 py-2 rounded-lg font-semibold bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-black hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300 hover:cursor-pointer" />
+                        <input type="submit" value={'Submit the assignment'} className="px-3 py-2 rounded-lg font-semibold bg-inherit text-gray-700 btn-border relative hover:bg-gradient-to-r hover:from-gray-500 hover:to-gray-400 hover:text-white hover:ring-2 hover:ring-offset-2 hover:ring-gray-400 transition-all ease-out duration-300 hover:cursor-pointer" />
                     </form>
                 </div>
             </div>
